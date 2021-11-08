5aa1ed1562f3070b7a4fec243b96c3aeac2cea2c
sandra2597 <68648398+sandra2597@users.noreply.github.com>
2021-11-08 10:30:52 +0100

    added initialization for finnhub

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+import finnhub
+load_dotenv('.env')
 
 app = FastAPI()
+API_KEY = os.getenv('API_KEY')
+finnhub_client = finnhub.Client(api_key=API_KEY)
 
 
 @app.get("/")
 async def root():
+    print(finnhub_client.quote('AAPL'))
     return {"message": "Hello World"}
