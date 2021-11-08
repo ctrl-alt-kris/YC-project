e38aa92e545bb0d63f6eb676ac20eabdfeebfce6
Ben <kwalijkje@live.com>
2021-11-08 11:56:26 +0100

    Added base models, schemas, and add user function to main.py

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,32 @@
-from fastapi import FastAPI
-import finnhub
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+# import finnhub
+from database import get_db, engine
+import schemas, models
 
+models.Base.metadata.create_all(engine)
 
 app = FastAPI()
 
 finnhub_api = "c64eft2ad3i8bn4fjpn0"
 
 # Setup client
-finnhub_client = finnhub.Client(api_key=finnhub_api)
+# finnhub_client = finnhub.Client(api_key=finnhub_api)
 
 # Quote
-quote = finnhub_client.quote('AAPL')
-current_price = quote["c"]
-closing_price = quote["pc"]
+# quote = finnhub_client.quote('AAPL')
+# current_price = quote["c"]
+# closing_price = quote["pc"]
 
-print("Current Price:", current_price)
-print("Closing price: ", closing_price)
+# print("Current Price:", current_price)
+# print("Closing price: ", closing_price)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/post_user")
+async def add_new_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return models.User.create(db, **user.dict())
---
+++ backend/models.py
@@ -0,0 +1,67 @@
+import sqlalchemy as sa
+from sqlalchemy.orm import Session, relationship
+from sqlalchemy.sql import schema
+from sqlalchemy.sql.schema import ForeignKey
+from pydantic import BaseModel
+from typing import Optional
+import schemas
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class Base(declarative_base()):
+    # never instatiate this class directly, needed for id column
+    __abstract__ = True
+
+    # all models should have an id
+    id = sa.Column(sa.Integer, primary_key=True, index=True)
+
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower() + "s"
+
+    @classmethod
+    def create(cls, session: Session, **kwargs):
+        obj = cls(**kwargs) # make obj out of arguments
+        session.add(obj)
+        session.commit()
+        session.refresh(obj)
+
+        return obj # need to return the obj
+
+class User(Base):
+    username = sa.Column(sa.String, unique=True, index=True)
+    firstName = sa.Column(sa.String, index=True)
+    lastName = sa.Column(sa.String, index=True)
+    email = sa.Column(sa.String, index=True)
+    password = sa.Column(sa.String, index=True)
+
+    # portfolios = relationship("Portfolio", back_populates="users")
+
+
+    @classmethod
+    def create(cls, session: Session, **kwargs):
+        kwargs["password"] = pwd_context.hash(kwargs["password"])
+        return super(User, cls).create(session, **kwargs)
+
+
+    @classmethod
+    def verify_password(cls, password):
+        return pwd_context.verify(password, cls.password)
+
+
+
+# class Portfolio(Base):
+#     name = sa.Column(sa.String, nullable=False)
+#     user_id = sa.Column(sa.Integer, sa.ForeignKey("users.id"), nullable=False)
+    
+#     users = relationship("User", back_populates="portfolios")
+#     transactions = relationship("Transaction", back_populates="portfolios")
+
+#     def get_value(self):
+#         return sum(t.amount for t in self.transactions)
+
+#     @property
+#     def owner(self):
+#         return self.user.email
