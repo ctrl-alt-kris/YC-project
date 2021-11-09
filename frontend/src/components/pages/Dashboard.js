2f61dc7bf8eba844ecb708bd567aaa543e9f8cd8
Bas van Hoof <bashooff@hotmail.com>
2021-11-09 17:08:31 +0100

    portfolio overview

--- frontend/src/components/pages/Dashboard.js
+++ frontend/src/components/pages/Dashboard.js
@@ -7,10 +7,22 @@ const cryptoData = MockDataCrypto
 
 // get all symbols in an array
 const stockSymbols = [];
+const costs = []
+
 stockData.forEach(element => stockSymbols.push(element["Symbol"]))
+stockData.forEach(element => costs.push(element['CostPrice']*element["Volume"]))
+
+const totalStockCosts = costs.reduce(function(costs, b) { return costs + b; }, 0);
 
 const cryptoNames = []
+const cryptoCosts = []
+
 cryptoData.forEach(element => cryptoNames.push(element["name"]))
+cryptoData.forEach(element => cryptoCosts.push(element['CostPrice']*element["Volume"]))
+
+const totalCryptoCosts = cryptoCosts.reduce(function(cryptoCosts, b) { return cryptoCosts + b; }, 0);
+
+const totalPortfolioCosts = (totalStockCosts + totalCryptoCosts).toFixed(2)
 
 
 const apiKey = "c64eft2ad3i8bn4fjpn0"
@@ -55,42 +67,56 @@ const Dashboard = () => {
     const closingPrices = {}
     const tickers = []
     const prices = []
+    const differences = []
 
     for (let i = 0; i < stockSymbols.length; i++) {
         if(quotes[stockSymbols[i]]!== undefined) {
          tickers.push(stockSymbols[i])
          prices.push(quotes[stockSymbols[i]]['c'])
+         differences.push((quotes[stockSymbols[i]]['c'] - stockData[i]['CostPrice'])*stockData[i]["Volume"])
         
         tickers.forEach((key, i) => closingPrices[key] = prices[i])
         }
     }
+
+    const totalStockDifferences = differences.reduce(function(differences, b) { return differences + b; }, 0);
     
     
     // create object for all current crypto prices
     const cryptoPrices = {}
     const names = []
     const cryptoPricesArray = []
+    const cryptoDifferences = []
     
     for (let i = 0; i < cryptoNames.length; i++) {
         if(cryptos[cryptoNames[i]]!== undefined) {
             names.push(cryptoNames[i])
             cryptoPricesArray.push(cryptos[cryptoNames[i]][cryptoNames[i]]["usd"])
             names.forEach((key, i) => cryptoPrices[key] = cryptoPricesArray[i])
+            cryptoDifferences.push((cryptos[cryptoNames[i]][cryptoNames[i]]["usd"] - cryptoData[i]['CostPrice'])*cryptoData[i]['Volume'])
         }
     }
+
+    const totalCryptoDifferences = cryptoDifferences.reduce(function(cryptoDifferences, b) { return cryptoDifferences + b; }, 0);
+
+    const totalPortfolioReturn = (totalStockDifferences + totalCryptoDifferences).toFixed(2)
+
+    console.log(totalPortfolioReturn)
 
 
     return(
         
             <div className="container">
-                <div class="card mt-5">
+                <div class="card mt-5 shadow p-3 mb-2 bg-white rounded">
                     <div class="card-body">
-                        Portfolio overview
+                        <h4>Portfolio overview</h4>
+                        <h5>Starting Balance: {totalPortfolioCosts}</h5>
+                        <h5>Total gains: {totalPortfolioReturn}</h5>
                     </div>
                 </div>
             <div className="row">
                 <div className="col-6">
-                    <div className="card mt-2">
+                    <div className="card mt-2 shadow p-3 mb-5 bg-white rounded">
                     <table className="table caption-top">
                         <caption>Stocks / ETFs (price in $)</caption>
                         <thead className="table-dark">
@@ -120,7 +146,7 @@ const Dashboard = () => {
                     </div>
                 </div>
                 <div className="col-6">
-                    <div className="card mt-2">
+                    <div className="card mt-2 shadow p-3 mb-5 bg-white rounded">
                     <table className="table caption-top">
                         <caption>Crypto Currencies (price in $)</caption>
                         <thead className="table-dark">
