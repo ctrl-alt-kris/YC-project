d84b19e2f98ce707f23a28e426fca8506778ecf5
sandra2597 <68648398+sandra2597@users.noreply.github.com>
2021-11-16 16:03:27 +0100

    fixed bugs

--- frontend/src/components/ui/Navbar.js
+++ frontend/src/components/ui/Navbar.js
@@ -1,14 +1,21 @@
 import "./Navbar.css"
 import { VscAccount } from "react-icons/vsc"
+import { useContext } from "react"
+import { DataContext } from "../../utils/DataContext"
 
-const Header = () => {
+const Header = (props) => {
+  const {activePage} = useContext(DataContext)
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
     return(
       
         <div className="normal-header-container">
            <nav class="navbar navbar-expand-lg navbar-light">
-            <div className="navbar-brand">Portfolio</div>
-              <div className="navbar-logo">
-              <VscAccount />
+            <div className="navbar-brand">{capitalizeFirstLetter(activePage)}</div>
+              <div className="navbar-logo" onClick={props.onLogout}>
+              Logout
             </div>
           </nav>
           <hr style={{ marginTop: "-10px"}}/>
