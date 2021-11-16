cd33696ba32045bfbae2041b0f0edff531c2d280
Ben <kwalijkje@live.com>
2021-11-16 10:38:51 +0100

    highest returns kind aworking

--- frontend/src/Components/Elements/HighestReturns.js
+++ frontend/src/Components/Elements/HighestReturns.js
@@ -1,7 +1,7 @@
 // symbol, cost-price*volume
 import mockDataStocks from "../Pages/mockDataStocks.json";
 import "./HighestReturns.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext, useRef } from "react";
 
 const apiKey = "c65764qad3i9pn79lmc0";
 
@@ -13,22 +13,13 @@ api_key.apiKey = apiKey; // Replace this
 const finnhubClient = new finnhub.DefaultApi();
 
 function HighestReturns() {
-  const [quotes, setQuotes] = useState({});
-  const stockData = mockDataStocks;
-  // get all symbols in an array
-  const stockSymbols = [];
+  const [stockData, setStockData] = useState(mockDataStocks);
 
-  stockData.forEach((element) => {
-    stockSymbols.push([
-      element["Symbol"],
-      element["CostPrice"],
-      element["Volume"],
-    ]);
-  });
-
-  // ROI = (((current price - purchase price) * volume) / (purchase price * volume)) * 100
+  // get current price for all stocks in stockData
+  let currentPrices = {};
 
   function calculateROI(currentPrice, purchasePrice, volume) {
+    // ROI = (((current price - purchase price) * volume) / (purchase price * volume)) * 100
     return (
       (((currentPrice - purchasePrice) * volume) / (purchasePrice * volume)) *
       100
@@ -39,43 +30,36 @@ function HighestReturns() {
     stockData.forEach((element) => {
       let symbol = element["Symbol"];
       finnhubClient.quote(symbol, (error, data, response) => {
-          console.log("in quotes: ", quotes);
-        setQuotes({ ...quotes, [symbol]: data.c });
+        currentPrices[symbol] = data.c;
+        element["ROI"] = calculateROI(
+          currentPrices[element["Symbol"]],
+          element["CostPrice"],
+          element["Volume"]
+        ).toFixed(2);
+        stockData.sort((a, b) => (a["ROI"] < b["ROI"] ? 1 : -1));
+        // setStockData([...stockData]);
       });
     });
-  }, []);
+  }, [currentPrices, stockData]);
 
-  console.log("quotes: ", quotes);
-  console.log(stockSymbols);
-
-  stockSymbols.forEach((element) => {
-    element.push(calculateROI(quotes[element["Symbol"]], element[1], element[2]));
-  });
-
-  console.log("With ROI added: ", stockSymbols);
-
-  console.log(stockData);
-
-  stockSymbols.sort((a, b) => (a[1] < b[1] ? 1 : -1));
-  console.log("THIS ONE HERE: ", stockSymbols);
+  console.log("sorted: ", stockData);
 
   return (
     <div>
       <div className="col-11 card mt-2 shadow p-3 mb-5 bg-white rounded flex-row">
-        {stockSymbols.map((elem) => {
-          if (stockSymbols.indexOf(elem) < 10) {
-            return (
-              // <div key={elem[0]} style={{margin: "auto", borderStyle: "solid", borderWidth: "1px", padding: "10px"}}>
-              <div key={elem[1]} style={{ margin: "auto", padding: "10px" }}>
-                <div style={{ textAlign: "center", fontWeight: "bold" }}>
-                  {elem[0]}
-                </div>
-                <div style={{ textAlign: "center" }}>{elem[1]}%</div>
+        {stockData.map((elem) => {
+          // if (x.indexOf(elem) < 10)
+          return (
+            <div
+              key={elem["Symbol"]}
+              style={{ margin: "auto", padding: "10px" }}
+            >
+              <div style={{ textAlign: "center", fontWeight: "bold" }}>
+                {elem["Symbol"]}
               </div>
-            );
-          } else {
-            return <div />;
-          }
+              <div style={{ textAlign: "center" }}>{elem["ROI"]}%</div>
+            </div>
+          );
         })}
       </div>
     </div>
